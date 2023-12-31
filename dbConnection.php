21e3fa052d4165451c2d7f812b53ca0a952eadc8
urdhanaka <urdhanaka.aptanagi@gmail.com>
2024-01-01 04:33:55 +0700

    first commit

---
+++ dbConnection.php
@@ -0,0 +1,8 @@
+<?php
+$databaseHost = 'localhost';
+$databaseName = 'latihan_crud';
+$databaseUsername = 'user';
+$databasePassword = '';
+
+// Open a new connection to the MySQL server
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
